5c097dae644f682761fe2f3b23d81e49156df57e
BongHwan Park <bongpark@example.com>
2026-04-18 22:56:49 +0900

    Initialize sermonnote cask v0.2.0

---
+++ Casks/sermonnote.rb
@@ -0,0 +1,18 @@
+cask "sermonnote" do
+  version "0.2.0"
+  sha256 "10d27b06308ec8d5c9d1977fc5d966ebb9fbd28889dfbaeb0db0586a4c26046f"
+
+  url "https://github.com/saintpbh/freehwp/releases/download/v0.2.0/sermonnote.dmg"
+  name "맘편한설교노트"
+  desc "에어갭 환경과 오프라인을 위한 HWP/HWPX 호환 성경 타이핑 에디터"
+  homepage "https://github.com/saintpbh/freehwp"
+
+  app "맘편한설교노트.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.tauri.sermonnote",
+    "~/Library/Caches/com.tauri.sermonnote",
+    "~/Library/Preferences/com.tauri.sermonnote.plist",
+    "~/Library/WebKit/com.tauri.sermonnote",
+  ]
+end
