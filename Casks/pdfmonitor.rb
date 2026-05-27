df48e9f6e6ea375e3d2aec9f82f5a71baca60782
BongHwan Park <bongpark@example.com>
2026-05-27 12:36:40 +0900

    bump: prok pdf studio Cask to v0.3.3

--- Casks/pdfmonitor.rb
+++ Casks/pdfmonitor.rb
@@ -1,16 +1,16 @@
 cask "pdfmonitor" do
   version "0.3.3"
-  sha256 "466c7670d8b5de9e20a611ddd34142f5fbd1e9b0151162edc19ed4ed50566383"
+  sha256 "065a491fc36c3a78b8fe70fdad7f9648abec15a5981a225597a6bd11331bf270"
 
-  url "https://github.com/saintpbh/PDFMonitor/releases/download/v#{version}/PDFMonitor-macOS.zip"
-  name "PDFMonitor"
+  url "https://github.com/saintpbh/PDFMonitor/releases/download/v#{version}/PROK%20PDF%20STUDIO-macOS.zip"
+  name "PROK PDF STUDIO"
   desc "High-performance presentation prompter and dual-screen PDF overlay presenter"
   homepage "https://github.com/saintpbh/PDFMonitor"
 
-  app "PDFMonitor.app"
+  app "PROK PDF STUDIO.app"
 
   zap trash: [
-    "~/Library/Application Support/PDFMonitor",
-    "~/Library/Preferences/com.antigravity.pdf.studio.plist",
+    "~/Library/Application Support/PROK PDF STUDIO",
+    "~/Library/Preferences/com.prok.pdf.studio.plist",
   ]
 end
