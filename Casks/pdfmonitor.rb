d6d32104759b17d4db5323662adf26755ba8a939
BongHwan Park <bongpark@example.com>
2026-06-10 15:16:28 +0900

    Bump PROK PDF STUDIO to v0.3.5 with postflight quarantine bypass

--- Casks/pdfmonitor.rb
+++ Casks/pdfmonitor.rb
@@ -1,6 +1,6 @@
 cask "pdfmonitor" do
-  version "0.3.4"
-  sha256 "5b9dcfd81739a5d42bed39144bbf86960268608e239363f2482de70e6f192261"
+  version "0.3.5"
+  sha256 "847e50099cea9d8287a20f33e57eed5072eb6c6355bc20afd4c8c7c3c7991b68"
 
   url "https://github.com/saintpbh/PDFMonitor/releases/download/v#{version}/PROK%20PDF%20STUDIO-macOS.zip"
   name "PROK PDF STUDIO"
@@ -8,6 +8,12 @@ cask "pdfmonitor" do
   homepage "https://github.com/saintpbh/PDFMonitor"
 
   app "PROK PDF STUDIO.app"
+
+  postflight do
+    system_command "xattr",
+                   args: ["-cr", "#{appdir}/PROK PDF STUDIO.app"],
+                   sudo: false
+  end
 
   zap trash: [
     "~/Library/Application Support/PROK PDF STUDIO",
